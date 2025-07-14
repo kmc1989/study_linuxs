eb5c6ddae4213df49bbcaadf163698a9885b859a
kmc1989 <55150100+kmc1989@users.noreply.github.com>
2025-07-14 18:00:38 +0900

    cd_pwd_ls 퀘스트

---
+++ codes/cd_pwd_ls.sh
@@ -0,0 +1,11 @@
+[mincheolkim@localhost ~]$ ls
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
+[mincheolkim@localhost ~]$ cd /home/mincheolkim/Music
+[mincheolkim@localhost Music]$ ls
+[mincheolkim@localhost Music]$ pwd
+/home/mincheolkim/Music
+[mincheolkim@localhost Music]$ cd /home/mincheolkim/Pictures
+[mincheolkim@localhost Pictures]$ pwd
+/home/mincheolkim/Pictures
+[mincheolkim@localhost Pictures]$ ls
+[mincheolkim@localhost Pictures]$ 
