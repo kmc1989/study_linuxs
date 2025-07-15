939606094212964fe3c6f906af0a3c569eadce5f
kmc1989 <55150100+kmc1989@users.noreply.github.com>
2025-07-15 12:23:10 +0900

    두번째 올리기

---
+++ codes/10_powershells.sh
@@ -0,0 +1,35 @@
+PS C:\> cd Develops
+PS C:\Develops> cd ..
+PS C:\> cd C:\Develops
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14  오전 11:55                Downloads
+d-----      2025-07-15  오전 11:14                VMs
+
+
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
+
+
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14  오전 11:55                Downloads
+d-----      2025-07-15  오전 11:14                VMs
+
+
+PS C:\Develops>
